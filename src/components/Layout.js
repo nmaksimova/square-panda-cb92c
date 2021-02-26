9302d19aee57c469c16dd4e6b2a8623f0cfa844d
nmaksimova <nataliia.maksimova@gmail.com>
2021-02-25 19:09:19 -0500

    added seo part

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,11 +17,30 @@ export default class Body extends React.Component {
             <React.Fragment>
                 <SEO />
                 <Helmet>
-                    <title>{_.get(this.props, 'pageContext.frontmatter.title', null) && (_.get(this.props, 'pageContext.frontmatter.title', null) + ' | ')}{_.get(this.props, 'pageContext.site.siteMetadata.title', null)}</title>
+                    <title>{_.get(this.props, 'pageContext.frontmatter.seo.title', null) ? (_.get(this.props, 'pageContext.frontmatter.seo.title', null)) : _.get(this.props, 'pageContext.frontmatter.title', null) + ' | ' + _.get(this.props, 'pageContext.site.siteMetadata.title', null)}</title>
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
-                    <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt', null) || _.get(this.props, 'pageContext.site.siteMetadata.description', null)}/>
+                    <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.seo.description', null) || ''} />
+                    {_.get(this.props, 'pageContext.frontmatter.seo.robots', null) && (
+                    <meta name="robots" content={_.join(_.get(this.props, 'pageContext.frontmatter.seo.robots', null), ',')}/>
+                    )}
+                    {_.map(_.get(this.props, 'pageContext.frontmatter.seo.extra', null), (meta, meta_idx) => {
+                        let key_name = _.get(meta, 'keyName', null) || 'name';
+                        return (
+                          _.get(meta, 'relativeUrl', null) ? (
+                            _.get(this.props, 'pageContext.site.siteMetadata.domain', null) && ((() => {
+                                let domain = _.trim(_.get(this.props, 'pageContext.site.siteMetadata.domain', null), '/');
+                                let rel_url = withPrefix(_.get(meta, 'value', null));
+                                let full_url = domain + rel_url;
+                                return (
+                                  <meta key={meta_idx} {...(attribute(key_name, _.get(meta, 'name', null)))} content={full_url}/>
+                                );
+                            })())
+                          ) : 
+                            <meta key={meta_idx + '.1'} {...(attribute(key_name, _.get(meta, 'name', null)))} content={_.get(meta, 'value', null)}/>
+                        )
+                    })}
                     {(style === 'bold') ? (
                       (font === 'serif') ? (
                       <link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/> 
